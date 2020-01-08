53c4ca4d8e92718f40f85d6c4fe2fa31b6b4ca41
John Hightshue <john.hightshue@gmail.com>
2020-01-08 15:46:16 -0800

    Creating a Game Initialization Function

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -11,16 +11,7 @@ GAME RULES:
 
 let scores, roundScore, activePlayer
 
-scores = [0, 0]
-roundScore = 0
-activePlayer = 1
-
-document.querySelector('.dice').style.display = 'none'
-
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+init()
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
@@ -63,6 +54,31 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         nextPlayer()
     }
 })
+
+document.querySelector('.btn-new').addEventListener('click', init)
+
+function init() {
+    scores = [0, 0]
+    roundScore = 0
+    activePlayer = 1
+
+    document.querySelector('.dice').style.display = 'none'
+
+    document.getElementById('score-0').textContent = '0'
+    document.getElementById('score-1').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.getElementById('name-0').textContent = 'Player 1'
+    document.getElementById('name-1').textContent = 'Player 2'
+
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.add('active')
+}
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
