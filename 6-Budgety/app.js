5d5091bb4647954f828a7880e764b543202d2d81
John Hightshue <john.hightshue@gmail.com>
2020-01-29 13:33:44 -0800

    Delete item from budget controller data structure

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -62,6 +62,20 @@ let budgetController = (function() {
             return newItem
         },
 
+        deleteItem: function(type, id) {
+            let ids, index
+
+            ids = data.allItems[type].map(function(current) {
+                return current.id
+            })
+
+            index = ids.indexOf(id)
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1)
+            }
+        },
+
         calculateBudget: function() {
 
             // calculate total income and expenses
@@ -255,10 +269,11 @@ let controller = (function(budgetCtrl, UICtrl) {
         if (itemID) {
             splitID = itemID.split('-')
             type = splitID[0]
-            ID = splitID[1]
+            ID = parseInt(splitID[1])
         }
 
         // 1. delete the item from the data structure
+        budgetCtrl.deleteItem(type, ID)
 
         // 2. delete the item from the UI
 
