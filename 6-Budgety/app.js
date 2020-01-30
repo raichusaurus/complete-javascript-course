467a1f15f3275ef216617763d44478ad543b5327
John Hightshue <john.hightshue@gmail.com>
2020-01-29 16:24:07 -0800

    Displaying the current month and year

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -153,7 +153,8 @@ let UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercentageLabel: '.item__percentage'
+        expensesPercentageLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     }
 
     let formatNumber = function(number, type) {
@@ -286,6 +287,20 @@ let UIController = (function() {
 
         },
 
+        displayMonth: function() {
+            let now, months, year
+
+            now = new Date()
+            // let christmas = new Date(2020, 12, 25)
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                'October', 'November', 'December']
+            month = now.getMonth()
+
+            year = now.getFullYear()
+            document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year
+        },
+
         getDOMStrings: function() {
             return DOMStrings
         }
@@ -387,6 +402,7 @@ let controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.')
+            UICtrl.displayMonth()
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
